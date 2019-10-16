3d66c3fdf8cc2a4bf2a81cd719b4b2e055fa8eb2
Arti <artijaraut711@gmail.com>
2019-10-16 11:16:06 +0530

    committing this file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to Deepak, Divya, Sneha etc..");
+      System.out.println("Hello all, Welcome to Aarti, Deepak, Divya, Sneha etc..");
+      System.out.println("Second hello");
+      System.out.println("Git staging");
    }
 }
